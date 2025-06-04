5dae33c331567a95a6555373f7697adc1cde3c0b
Haider Rizvi <rizvihaider0801@icloud.com>
2025-06-04 10:38:06 +0530

    Fist commit for initial setup

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() 
+{
+    return
+    (
+        <main className="p-10">
+            <h1 className="text-4xl font-bold">Contact</h1>
+            <p className="mt-4 text-lg">I’m Haider Rizvi, a data & tech enthusiast...</p>
+        </main>
+    )
+}
